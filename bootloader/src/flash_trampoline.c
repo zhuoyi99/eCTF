cbf7a2806b43ab3d0e3ed69b15c59ec440481302
sera-sch <jzchuang@uci.edu>
2022-03-08 06:12:31 -0800

    Make flash_trampoline start at a proper place

--- bootloader/src/flash_trampoline.c
+++ bootloader/src/flash_trampoline.c
@@ -4,17 +4,10 @@
 #include "uart.h"
 
 __attribute__((section(".data"))) void current_hash(uint8_t* out, uint8_t* start, uint32_t size) {
-    /*
-    struct tc_sha256_state_struct hash;
-    tc_sha256_init(&hash);
-    tc_sha256_update(&hash, (uint8_t*)0x4, (uint32_t)start - 0x4); // 0x4, start
-    tc_sha256_update(&hash, start + size, 0x40000 - (uint32_t)(start + size)); // start+size, 0x40000
-    tc_sha256_final(out, &hash);
-    */
     SHA256_CTX ctx;
     sha256_init(&ctx);
-    sha256_update(&ctx, 0, (uint32_t)start);
-    // sha256_update(&ctx, start + size, 0x40000 - (uint32_t)(start + size));
+    sha256_update(&ctx, 0x5800, (uint32_t)start - 0x5800);
+    sha256_update(&ctx, start + size, 0x40000 - (uint32_t)(start + size));
     sha256_final(&ctx, out);
 }
 
